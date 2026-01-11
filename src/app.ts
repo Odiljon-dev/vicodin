7911b04d676e292f6a5c8a9ce073e1ff6cbd6daa
Odiljon <odiljonzokirov551@gmail.com>
2026-01-11 22:51:23 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin"
 
 /** 1-ENTRAMCE **/
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 /** SESSIONS**/
 
 /** VIEWS **/
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** ROUTERS **/
-app.use("/", router);  // Middleware Design pattern
+app.use("/admin", routerAdmin); // SSR: EJS
+app.use("/", router); // REACT
 
 export default app;
