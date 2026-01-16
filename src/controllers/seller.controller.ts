d86f631c15355ee90a52ca41337f9f1bccb4ab5b
Odiljon <odiljonzokirov551@gmail.com>
2026-01-16 11:23:34 +0900

    feat: create project standards

--- src/controllers/seller.controller.ts
+++ src/controllers/seller.controller.ts
@@ -6,6 +6,7 @@ import { T } from "../libs/types/common";
 const sellerController: T = {};
 sellerController.goHome = (req: Request, res: Response) => {
   try {
+    console.log('goHome');
     res.send(" Home Page");
   } catch (err) {
     console.log("Error, goHome:", err);
@@ -14,6 +15,7 @@ sellerController.goHome = (req: Request, res: Response) => {
 
 sellerController.getLogin = (req: Request, res: Response) => {
   try {
+    console.log('getLogin');
     res.send(" Login Page");
   } catch (err) {
     console.log("Error, getLogin:", err);
@@ -22,6 +24,7 @@ sellerController.getLogin = (req: Request, res: Response) => {
 
 sellerController.getSignup = (req: Request, res: Response) => {
   try {
+    console.log('getSignup');
     res.send(" Signup Page");
   } catch (err) {
     console.log("Error, getSignup:", err);
---
+++ train.ts
@@ -0,0 +1,9 @@
+/* Project Standards: 
+- Logging standards morgan orqali berilgan vaqtni hosil qilyapmiz
+- Naming standards 
+function, method, variable => bu CAMEL case goHome
+class => PASCAL MemberService
+folder => KEBAB
+css => SNAKE button_style
+- Error handling 
+*/
