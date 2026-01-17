fbbcad21421919b24a6d1b9c68af34c47c643e48
Odiljon <odiljonzokirov551@gmail.com>
2026-01-17 11:19:44 +0900

    feat: develop seller login procces

--- src/controllers/seller.controller.ts
+++ src/controllers/seller.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import MemberService from "../models/Member.service";
 import { T } from "../libs/types/common";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const sellerController: T = {};
@@ -33,12 +33,19 @@ sellerController.getSignup = (req: Request, res: Response) => {
   }
 };
 
-sellerController.processLogin = (req: Request, res: Response) => {
+sellerController.processLogin = async (req: Request, res: Response) => {
   try {
     console.log("processLogin");
-    res.send("DONE");
+    const input: LoginInput = req.body;
+       console.log("body:", req.body);
+
+       const memberService = new MemberService();
+       const result= await memberService.proccesLogin(input);
+
+    res.send(result);
   } catch (err) {
     console.log("Error, processLogin:", err);
+    res.send(err);
   }
 };
 
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,5 @@
 import MemberModel from "../schema/Member.model";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/enums/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 
@@ -22,6 +22,24 @@ class MemberService {
     } catch (err) {
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
+  }
+
+  public async proccesLogin(input: LoginInput): Promise<Member> {
+    const member = await this.memberModel
+      .findOne(
+        { memberNick: input.memberNick },
+        { memberNick: 1, memberPassword: 1 },
+      )
+      .exec();
+    if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+    const isMatch = input.memberPassword === member.memberPassword;
+    console.log("isMatch:", isMatch);
+
+    if (!isMatch) {
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    }
+    return await this.memberModel.findById(member._id).exec();
   }
 }
 
