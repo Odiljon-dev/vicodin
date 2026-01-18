f74b20a357b2b839f85566e19c22104e0cb05391
Odiljon <odiljonzokirov551@gmail.com>
2026-01-18 11:17:17 +0900

    feat: frontend related css, js and image creation

--- src/controllers/seller.controller.ts
+++ src/controllers/seller.controller.ts
@@ -10,7 +10,7 @@ const sellerController: T = {};
 sellerController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send(" Home Page");
+    res.render("home");
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -19,7 +19,7 @@ sellerController.goHome = (req: Request, res: Response) => {
 sellerController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send(" Signup Page");
+    res.render("signup");
   } catch (err) {
     console.log("Error, getSignup:", err);
   }
@@ -28,7 +28,7 @@ sellerController.getSignup = (req: Request, res: Response) => {
 sellerController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send(" Login Page");
+    res.render("login");
     // send | json | redirect | end | render
   } catch (err) {
     console.log("Error, getLogin:", err);
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,8 @@ mongoose
     console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
-      console.log(`The sever is running successfully on port: ${PORT}`);
+      console.info(`The sever is running successfully on port: ${PORT}`);
+      console.info(`Admin project on http://localhost:${PORT}/admin \n`);
     });
   })
   .catch((err) => console.log("Error on connection MongoDB", err));
