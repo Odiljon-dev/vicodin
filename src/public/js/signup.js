6b24f74cafc529af412986b116ca8a90d8382e09
Odiljon <odiljonzokirov551@gmail.com>
2026-01-23 00:26:40 +0900

    feat: signup process frontand validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,24 @@
 console.log("Signup frontend javascript file");
+
+
+function validateSignupForm() {
+  const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
+
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    alert("Please insert all required inputs!");
+    return false;
+  }
+
+  if (memberPassword !== confirmPassword) {
+    alert("Password differs, please check!");
+    return false;
+  }
+}
